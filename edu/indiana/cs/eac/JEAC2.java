282436b62e904bf4ca09a94de554ceb9592aa9a1
ryanvarick <github@ryanvarick.com>
2007-02-20 13:41:27 +0000

    - Created a new GUI package for 2.0-series GUI development - Stripped JEAC2 down to a main() method - Bootstrapped the MDIManager class

--- edu/indiana/cs/eac/JEAC2.java
+++ edu/indiana/cs/eac/JEAC2.java
@@ -1,102 +1,54 @@
-//based on http://www.javaworld.com/javaworld/jw-05-2001/jw-0525-mdi.html
+/*
+ * This file is part of jEAC (http://jeac.sf.net/).
+ * 
+ * Copyright (C) 2007.  All rights reserved.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ * 
+ */
 
 package edu.indiana.cs.eac;
 
-import javax.swing.*;
+import edu.indiana.cs.eac.gui.*;
 
-import com.sun.java.swing.SwingUtilities2;
-
-import java.awt.event.*;
-import java.awt.*;
-
-import edu.indiana.cs.eac.testing.ui.*;
-import ec.display.*;
-
-public class JEAC2 extends JFrame
+/**
+ * A development environment for the extended analog computer.
+ * 
+ * <p>This is the top-level class for the jEAC user interface.  It simply
+ * instantiates the MDI manager and handles other bootstrapping functions.
+ * 
+ * @author   Ryan R. Varick
+ * @since    1.3.0
+ * 
+ */
+public class JEAC2
 {
-	private MDIDesktopPane desktop;
+	/** Initial size of the UI. */
+	public static int INITIAL_SIZE_X = 640;
+	
+	/** Initial size of the UI. */
+	public static int INITIAL_SIZE_Y = 480;
 
-	private JMenuBar menuBar;
 
-	private JMenu fileMenu;
+	
+	/* -------------------------[ Generic class methods ]------------------------- */
 
-	private JMenuItem newMenu;
-
-	private JScrollPane scrollPane;
-
-	public JEAC2() {
-
-		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (Exception e) {
-			System.err.println("Unable to load native look and feel.");
-		}
-
-		desktop = new MDIDesktopPane();
-		desktop.setBackground(Color.GRAY);
-
-		menuBar = new JMenuBar();
-
-		// it don't work
-		//		menuBar.putClientProperty(SwingUtilities2.AA_TEXT_PROPERTY_KEY, new Boolean(true));
-
-		fileMenu = new JMenu("jEAC");
-		JMenuItem newMenu = new JMenuItem("New");
-		JScrollPane scrollPane = new JScrollPane();
-
-		menuBar.add(fileMenu);
-
-		JMenuItem ecj = new JMenuItem("ECJ");
-		ecj.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent ae) {
-//				desktop.add(new Console(new String[0]));
-				
-				Console c = new Console(new String[0]);
-				c.setVisible(true);
-				
-			}});
-		
-		JMenuItem jeac = new JMenuItem("jEAC");
-		jeac.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent ae) {
-//				desktop.add(new Console(new String[0]));
-				JEAC j = new JEAC();
-			}});
-		
-		JMenu tools = new JMenu("Tools");
-		tools.add(ecj);
-		tools.add(jeac);
-		
-		
-		menuBar.add(tools);
-
-		menuBar.add(new WindowMenu(desktop));
-		fileMenu.add(newMenu);
-
-		menuBar.add(new JMenu("Help"));
-
-		setJMenuBar(menuBar);
-		this.setLocation(new Point(50,50));
-		setTitle("jEAC - An integrated EAC development suite");
-		scrollPane.getViewport().add(desktop);
-		getContentPane().setLayout(new BorderLayout());
-		getContentPane().add(scrollPane, BorderLayout.CENTER);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		// this how we register with the MDI manager
-		newMenu.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent ae) {
-				desktop.add(new TextFrame());
-			}
-		});
-
-	}
-
+	/**
+	 * Entry-point for the MDI interface.
+	 * 
+	 * @param args   Command line arguments are ignored.
+	 * 
+	 * @author       Ryan R. Varick
+	 * @since        1.3.0
+	 * 
+	 */
 	public static void main(String[] args)
 	{
-		JEAC2 jeac = new JEAC2();
-		jeac.setSize(600, 400);
-		jeac.setVisible(true);
+		MDIManager app = new MDIManager();
+		app.setSize(INITIAL_SIZE_X, INITIAL_SIZE_Y);
+		app.setVisible(true);
 	}
-
 }
---
+++ edu/indiana/cs/eac/gui/MDIManager.java
@@ -0,0 +1,115 @@
+/*
+ * This file is part of jEAC (http://jeac.sf.net/).
+ * 
+ * Copyright (C) 2007.  All rights reserved.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ * 
+ */
+
+//based on http://www.javaworld.com/javaworld/jw-05-2001/jw-0525-mdi.html
+
+package edu.indiana.cs.eac.gui;
+
+import javax.swing.*;
+
+import java.awt.event.*;
+import java.awt.*;
+
+import edu.indiana.cs.eac.testing.ui.*;
+import ec.display.*;
+
+/**
+ * Multi-document interface (MDI) manager.
+ * 
+ * <p>This class implements an MDI manager.  
+ * 
+ * @author Ryan R. Varick
+ * @since  1.3.0
+ *
+ */
+public class MDIManager extends JFrame
+{
+	private MDIDesktopPane desktop;
+	private JScrollPane scrollPane;
+
+	private JMenuBar menuBar;
+	private JMenu fileMenu;
+	private JMenuItem newMenu;
+
+	public MDIManager()
+	{
+
+		try
+		{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} 
+		catch(Exception e)
+		{
+			System.err.println("Unable to load native look-and-feel.");
+		}
+
+		desktop = new MDIDesktopPane();
+		desktop.setBackground(Color.GRAY);
+
+		menuBar = new JMenuBar();
+
+
+		fileMenu = new JMenu("jEAC");
+		JMenuItem newMenu = new JMenuItem("New");
+		JScrollPane scrollPane = new JScrollPane();
+
+		menuBar.add(fileMenu);
+
+		JMenuItem ecj = new JMenuItem("ECJ");
+		ecj.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+//				desktop.add(new Console(new String[0]));
+				
+				Console c = new Console(new String[0]);
+				c.setVisible(true);
+				
+			}});
+		
+		JMenuItem jeac = new JMenuItem("jEAC");
+		jeac.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+//				desktop.add(new Console(new String[0]));
+//				JEAC j = new JEAC();
+			}});
+		
+		JMenu tools = new JMenu("Tools");
+		tools.add(ecj);
+		tools.add(jeac);
+		
+		
+		menuBar.add(tools);
+
+		menuBar.add(new WindowMenu(desktop));
+		fileMenu.add(newMenu);
+
+		menuBar.add(new JMenu("Help"));
+
+		setJMenuBar(menuBar);
+		this.setLocation(new Point(50,50));
+		setTitle("jEAC - An integrated EAC development suite");
+		scrollPane.getViewport().add(desktop);
+		getContentPane().setLayout(new BorderLayout());
+		getContentPane().add(scrollPane, BorderLayout.CENTER);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// this how we register with the MDI manager
+		newMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				desktop.add(new TextFrame());
+			}
+		});
+
+	}
+
+
+
+}
